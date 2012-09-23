b2f8a415fa3e780481f72c8dd54fe06fbc208070
Yash Ranadive <yashr@Yashs-MacBook-Air.local>
2012-09-23 14:00:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prospect::Application.config.secret_token = 'cdd0b4186d63ba6d891d610e2c1df2ddc1b5f58a1760e6f98ca54f2e0f156adbd966eb394d9dd1394e79d4b8e19a5ea99d9e65819a5c946bc16ce718bef2556e'
